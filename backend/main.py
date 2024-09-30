fea12d3678839aff6b34e06f0593c72cdec2a33f
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2024-09-29 23:32:36 -0300

    feat: adding dummy CRUD routes

--- backend/main.py
+++ backend/main.py
@@ -16,3 +16,23 @@ app.add_middleware(
 app.get("/")
 def read_root():
     return {"name": "John Doe", "Message": "Hello World"}
+
+@app.get("/api/tasks")
+async def get_all_tasks():
+    return 1
+
+@app.get("/api/tasks/{id}")
+async def get_task_by_id(id: int):
+    return id
+
+@app.post("/api/tasks")
+async def create_task():
+    return 1
+
+@app.put("/api/tasks/{id}")
+async def update_task(id: int):
+    return id
+
+@app.delete("/api/tasks/{id}")
+async def delete_task(id: int):
+    return id
